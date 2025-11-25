e1e57b257932186725aeeb2e3d8b656e797c1f4e
Emmanuel Oghenenyore Olokor <neolokor1@gmail.com>
2025-11-25 23:07:08 +0100

    feat: init on ts conversion

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_API_URL: string;
+	// readonly VITE_PUBLIC_KEY?: string;
+	// add more env vars here...
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
